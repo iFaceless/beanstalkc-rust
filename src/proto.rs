52cc32b3e300f81d474cc0762e04c9ec62503b0a
ifaceless <christopherlee199398@gmail.com>
2019-02-13 16:36:40 +0800

    ✨ feat: finish command builder

--- src/proto.rs
+++ src/proto.rs
@@ -1,16 +1,134 @@
+use std::str::FromStr;
 use std::string::ToString;
 use std::time::Duration;
 
-use self::cmdkind::CommandKind;
-use self::status::Status;
+use crate::error::BeanstalkcError;
 
-mod cmdkind;
-mod status;
+#[derive(Debug)]
+pub enum CommandKind {
+    Put,
+    PeekJob,
+    PeekReady,
+    PeekDelayed,
+    PeekBuried,
+    Reserve,
+    ReserveTimeout,
+    Delete,
+    Release,
+    Bury,
+    Kick,
+    JobKick,
+    Touch,
+    Stats,
+    JobStats,
+    Use,
+    Watch,
+    Ignore,
+    ListTubes,
+    ListTubeUsed,
+    ListTubesWatched,
+    StatsTube,
+    Quit,
+    PauseTube,
+}
+
+impl ToString for CommandKind {
+    fn to_string(&self) -> String {
+        let cmd = match *self {
+            CommandKind::Put => "put",
+            CommandKind::PeekJob => "peek",
+            CommandKind::PeekReady => "peek-ready",
+            CommandKind::PeekDelayed => "peek-delayed",
+            CommandKind::PeekBuried => "peek-buried",
+            CommandKind::Reserve => "reserve",
+            CommandKind::ReserveTimeout => "reserve-with-timeout",
+            CommandKind::Delete => "delete",
+            CommandKind::Release => "release",
+            CommandKind::Bury => "bury",
+            CommandKind::Kick => "kick",
+            CommandKind::JobKick => "kick-job",
+            CommandKind::Touch => "touch",
+            CommandKind::Stats => "stats",
+            CommandKind::JobStats => "stats-job",
+            CommandKind::Use => "use",
+            CommandKind::Watch => "watch",
+            CommandKind::Ignore => "ignore",
+            CommandKind::ListTubes => "list-tubes",
+            CommandKind::ListTubeUsed => "list-tube-used",
+            CommandKind::ListTubesWatched => "list-tubes-watched",
+            CommandKind::StatsTube => "stats-tube",
+            CommandKind::Quit => "quit",
+            CommandKind::PauseTube => "pause-tube",
+        };
+        cmd.to_string()
+    }
+}
+
+#[derive(Debug)]
+pub enum Status {
+    Ok,
+    Found,
+    NotFound,
+    Reserved,
+    DeadlineSoon,
+    TimedOut,
+    Deleted,
+    Released,
+    Buried,
+    Kicked,
+    Using,
+    Watching,
+    Touched,
+    Inserted,
+    NotIgnored,
+    OutOfMemory,
+    InternalError,
+    Draining,
+    BadFormat,
+    UnknownCommand,
+    ExpectedCRLF,
+    JobTooBig,
+}
+
+impl FromStr for Status {
+    type Err = BeanstalkcError;
+
+    fn from_str(s: &str) -> Result<Self, BeanstalkcError> {
+        let s = match s {
+            "OK" => Status::Ok,
+            "FOUND" => Status::Found,
+            "NOT_FOUND" => Status::NotFound,
+            "RESERVED" => Status::Reserved,
+            "DEADLINE_SOON" => Status::DeadlineSoon,
+            "TIMED_OUT" => Status::TimedOut,
+            "DELETED" => Status::Deleted,
+            "RELEASED" => Status::Released,
+            "BURIED" => Status::Buried,
+            "KICKED" => Status::Kicked,
+            "USING" => Status::Using,
+            "WATCHING" => Status::Watching,
+            "TOUCHED" => Status::Touched,
+            "INSERTED" => Status::Inserted,
+            "NOT_IGNORED" => Status::NotIgnored,
+            "OUT_OF_MEMORY" => Status::OutOfMemory,
+            "INTERNAL_ERROR" => Status::InternalError,
+            "DRAINING" => Status::Draining,
+            "BAD_FORMAT" => Status::BadFormat,
+            "UNKNOWN_COMMAND" => Status::UnknownCommand,
+            "EXPECTED_CRLF" => Status::ExpectedCRLF,
+            "JOB_TOO_BIG" => Status::JobTooBig,
+            _ => {
+                return Err(BeanstalkcError::CommandFailed(s.to_string()));
+            }
+        };
+        Ok(s)
+    }
+}
 
 #[derive(Debug)]
 pub struct Command<'a> {
     kind: CommandKind,
-    args: Option<Vec<String>>,
+    args: Vec<String>,
     body: Option<&'a [u8]>,
     expected_ok_status: Vec<Status>,
     expected_error_status: Vec<Status>,
@@ -19,7 +137,7 @@ pub struct Command<'a> {
 impl<'a> Command<'a> {
     fn new(
         kind: CommandKind,
-        args: Option<Vec<String>>,
+        args: Vec<String>,
         body: Option<&'a [u8]>,
         exp_ok_status: Vec<Status>,
         exp_error_status: Vec<Status>,
@@ -39,8 +157,8 @@ impl<'a> Command<'a> {
 
         let mut cmd = self.kind.to_string();
 
-        if self.args.is_some() {
-            cmd = cmd + SPACE + self.args.clone().unwrap().join(SPACE).as_str();
+        if self.args.len() > 0 {
+            cmd = cmd + SPACE + self.args.clone().join(SPACE).as_str();
         }
 
         if self.body.is_some() {
@@ -64,13 +182,13 @@ impl<'a> Command<'a> {
 
     // Construct commands
     pub fn put(body: &'a [u8], priority: u32, delay: Duration, ttr: Duration) -> Self {
-        Self::new(
+        Command::new(
             CommandKind::Put,
-            Some(vec![
+            vec![
                 priority.to_string(),
                 delay.as_secs().to_string(),
                 ttr.as_secs().to_string(),
-            ]),
+            ],
             Some(body),
             vec![Status::Inserted],
             vec![Status::JobTooBig, Status::Buried, Status::Draining],
@@ -78,15 +196,188 @@ impl<'a> Command<'a> {
     }
 
     pub fn reserve(timeout: Option<Duration>) -> Self {
-        Self::new(
+        Command::new(
             match timeout {
                 None => CommandKind::Reserve,
                 Some(_) => CommandKind::ReserveTimeout,
             },
-            timeout.map(|t|vec![t.as_secs().to_string()]),
+            timeout
+                .map(|t| vec![t.as_secs().to_string()])
+                .unwrap_or(vec![]),
             None,
             vec![Status::Reserved],
             vec![Status::TimedOut, Status::DeadlineSoon],
         )
     }
+
+    pub fn kick(bound: u32) -> Self {
+        Command::new(
+            CommandKind::Kick,
+            vec![bound.to_string()],
+            None,
+            vec![Status::Kicked],
+            vec![],
+        )
+    }
+
+    pub fn kick_job(job_id: u64) -> Self {
+        Command::new(
+            CommandKind::JobKick,
+            vec![job_id.to_string()],
+            None,
+            vec![Status::Kicked],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn peek_job(job_id: u64) -> Self {
+        Self::peek(CommandKind::PeekJob, vec![job_id.to_string()])
+    }
+
+    pub fn peek_ready() -> Self {
+        Self::peek(CommandKind::PeekReady, vec![])
+    }
+
+    pub fn peek_delayed() -> Self {
+        Self::peek(CommandKind::PeekDelayed, vec![])
+    }
+
+    pub fn peek_buried() -> Self {
+        Self::peek(CommandKind::PeekBuried, vec![])
+    }
+
+    fn peek(kind: CommandKind, args: Vec<String>) -> Self {
+        Command::new(
+            kind,
+            args,
+            None,
+            vec![Status::Found],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn tubes() -> Self {
+        Command::new(
+            CommandKind::ListTubes,
+            vec![],
+            None,
+            vec![Status::Ok],
+            vec![],
+        )
+    }
+
+    pub fn using() -> Self {
+        Command::new(
+            CommandKind::ListTubeUsed,
+            vec![],
+            None,
+            vec![Status::Using],
+            vec![],
+        )
+    }
+
+    pub fn use_tube(name: &str) -> Self {
+        Command::new(
+            CommandKind::Use,
+            vec![name.to_string()],
+            None,
+            vec![Status::Using],
+            vec![],
+        )
+    }
+
+    pub fn watch(name: &str) -> Self {
+        Command::new(
+            CommandKind::Watch,
+            vec![name.to_string()],
+            None,
+            vec![Status::Watching],
+            vec![],
+        )
+    }
+
+    pub fn ignore(name: &str) -> Self {
+        Command::new(
+            CommandKind::Ignore,
+            vec![name.to_string()],
+            None,
+            vec![Status::Watching],
+            vec![Status::NotIgnored],
+        )
+    }
+
+    pub fn stats() -> Self {
+        Command::new(CommandKind::Stats, vec![], None, vec![Status::Ok], vec![])
+    }
+
+    pub fn stats_tube(name: &str) -> Self {
+        Command::new(
+            CommandKind::StatsTube,
+            vec![name.to_string()],
+            None,
+            vec![Status::Ok],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn pause_tube(name: &str) -> Self {
+        Command::new(
+            CommandKind::PauseTube,
+            vec![name.to_string()],
+            None,
+            vec![Status::Ok],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn delete(job_id: u64) -> Self {
+        Command::new(
+            CommandKind::Delete,
+            vec![job_id.to_string()],
+            None,
+            vec![Status::Deleted],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn release(job_id: u64, priority: u32, delay: Duration) -> Self {
+        Command::new(
+            CommandKind::Release,
+            vec![priority.to_string(), delay.as_secs().to_string()],
+            None,
+            vec![Status::Released, Status::Buried],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn bury(job_id: u64, priority: u32) -> Self {
+        Command::new(
+            CommandKind::Bury,
+            vec![job_id.to_string(), priority.to_string()],
+            None,
+            vec![Status::Buried],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn touch(job_id: u64) -> Self {
+        Command::new(
+            CommandKind::Touch,
+            vec![job_id.to_string()],
+            None,
+            vec![Status::Ok],
+            vec![Status::NotFound],
+        )
+    }
+
+    pub fn stats_job(job_id: u64) -> Self {
+        Command::new(
+            CommandKind::JobStats,
+            vec![job_id.to_string()],
+            None,
+            vec![Status::Ok],
+            vec![Status::NotFound],
+        )
+    }
 }
+
