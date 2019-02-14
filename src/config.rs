5715f89173fa1a8a61431f66ccd8a63569a00f1f
ifaceless <christopherlee199398@gmail.com>
2019-02-14 12:49:51 +0800

    🔥 clean: remove unsused consts

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,3 @@ pub const DEFAULT_CONNECTION_TIMEOUT: Option<Duration> = Some(Duration::from_sec
 pub const DEFAULT_JOB_PRIORITY: u32 = 1 << 31;
 pub const DEFAULT_JOB_TTR: Duration = Duration::from_secs(120);
 pub const DEFAULT_JOB_DELAY: Duration = Duration::from_secs(0);
-
-#[allow(dead_code)]
-pub const DEFAULT_TUBE_NAME: &str = "default";
